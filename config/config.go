be04ff2ec82e835e0e9b303bdf35fea9296c12fd
Mendel Greenberg <chabad360@gmail.com>
2020-08-18 23:55:50 -0400

    add config validation validation

--- config/config.go
+++ config/config.go
@@ -1,24 +1,26 @@
 package config
 
 import (
-	"github.com/BoRuDar/configuration/v2"
+	"github.com/BoRuDar/configuration/v3"
+	"github.com/go-playground/validator/v10"
+	"log"
 )
 
 var (
-	// Config is a struct that provides all covey configuration details.
+	// Config is a struct  that provides all covey configuration details.
 	Config = struct {
 		// revive:disable:line-length-limit
 		Daemon struct {
-			Host          string `default:"" flag:"host||Resolvable address for this machine (If empty covey will listen on every address)." env:"COVEY_HOST"`
-			Port          string `default:"8080" flag:"port||Port to expose the covey daemon on. (default '8080')" env:"COVEY_PORT"`
-			PluginsFolder string `default:"/usr/lib64/covey/plugins" flag:"plugins-folder||Folder where plugins are located. (default '/usr/lib64/covey/plugins')" env:"COVEY_PLUGINS_FOLDER"`
+			Host          string `default:"" flag:"host||Resolvable address for this machine (If empty covey will listen on every address)." env:"COVEY_HOST" validate:"omitempty,ip_addr"`
+			Port          string `default:"8080" flag:"port||Port to expose the covey daemon on. (default '8080')" env:"COVEY_PORT" validate:"number,required"`
+			PluginsFolder string `default:"/usr/lib64/covey/plugins" flag:"plugins-dir||Directory where plugins are located. (default '/usr/lib64/covey/plugins')" env:"COVEY_PLUGINS_DIRECTORY" validate:"dir,required"`
 		}
 		DB struct {
-			Username string `default:"postgres" flag:"postgres-username||The username used to login to the postgres database. (default 'postgres')" env:"COVEY_POSTGRES_USERNAME"`
+			Username string `default:"postgres" flag:"postgres-username||The username used to login to the postgres database. (default 'postgres')" env:"COVEY_POSTGRES_USERNAME" validate:"required"`
 			Password string `default:"" flag:"postgres-password||The password used to login to the postgres database." env:"COVEY_POSTGRES_PASSWORD"`
-			Host     string `default:"127.0.0.1" flag:"postgres-host||The Postgres host (default '127.0.0.1')" env:"COVEY_POSTGRES_HOST"`
-			Port     string `default:"5432" flag:"postgres-port||The Postgres port (default '5432')" env:"COVEY_POSTGRES_PORT"`
-			Database string `default:"covey" flag:"postgres-database||The database (default 'covey')" env:"COVEY_POSTGRES_DATABASE"`
+			Host     string `default:"localhost" flag:"postgres-host||The Postgres host (default 'localhost')" env:"COVEY_POSTGRES_HOST" validate:"required,hostname_rfc1123"`
+			Port     string `default:"5432" flag:"postgres-port||The Postgres port. (default '5432')" env:"COVEY_POSTGRES_PORT" validate:"required,number"`
+			Database string `default:"covey" flag:"postgres-database||The database. (default 'covey')" env:"COVEY_POSTGRES_DATABASE" validate:"required"`
 		}
 		// revive:enable:line-length-limit
 	}{}
@@ -26,16 +28,28 @@ var (
 
 // InitConfig initializes the configuration values.
 func InitConfig() error {
-	configurator, err := configuration.New(&Config, []configuration.Provider{
+	fp, err := configuration.NewFileProvider("/etc/covey/config.yml")
+	if err != nil {
+		log.Println(err)
+	}
+	configurator, err := configuration.New(&Config,
 		configuration.NewFlagProvider(&Config),
 		configuration.NewEnvProvider(),
-		configuration.NewFileProvider("/etc/covey/config.yml"),
+		fp,
 		configuration.NewDefaultProvider(),
-	}, false, false)
+	)
 	if err != nil {
 		return err
 	}
+	configurator.SetOnFailFn(func(err error) {
+		log.Println(err)
+	})
 
 	configurator.InitValues()
-	return nil
+	return validate(Config)
+}
+
+func validate(cfg interface{}) error {
+	val := validator.New()
+	return val.Struct(cfg)
 }
--- go.mod
+++ go.mod
@@ -1,9 +1,9 @@
 module github.com/chabad360/covey
 
-go 1.14
+go 1.15
 
 require (
-	github.com/BoRuDar/configuration/v2 v2.2.4
+	github.com/BoRuDar/configuration/v3 v3.0.1
 	github.com/bramvdbogaerde/go-scp v0.0.0-20200518191442-5c8efdd1d925
 	github.com/caarlos0/env/v6 v6.3.0
 	github.com/containerd/continuity v0.0.0-20200710164510-efbc4488d8fe // indirect
@@ -11,11 +11,18 @@ require (
 	github.com/go-playground/pkg/v5 v5.1.0
 	github.com/go-playground/pure/v5 v5.1.0
 	github.com/go-playground/validator/v10 v10.3.0
-	github.com/google/go-cmp v0.4.0
+	github.com/google/go-cmp v0.5.1
+	github.com/jackc/pgproto3/v2 v2.0.4 // indirect
 	github.com/json-iterator/go v1.1.10
+	github.com/magefile/mage v1.10.0 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/ory/dockertest/v3 v3.6.0
 	github.com/robfig/cron/v3 v3.0.1
-	golang.org/x/crypto v0.0.0-20200709230013-948cd5f35899
-	gorm.io/driver/postgres v0.2.5
-	gorm.io/gorm v0.2.27
+	golang.org/x/crypto v0.0.0-20200728195943-123391ffb6de
+	golang.org/x/sys v0.0.0-20200817155316-9781c653f443 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	gopkg.in/yaml.v2 v2.3.0 // indirect
+	gorm.io/driver/postgres v0.2.7
+	gorm.io/gorm v0.2.33
 )
